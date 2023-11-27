eb63b93c04d62dae4afb176d2ae11e62372a2760
DEMOHCHIK <smnvmaks@gmail.com>
2023-11-27 14:42:15 +0600

    Добавил модель + миграцию + регестрацию в админке

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    description = models.TextField(verbose_name='Описание', blank=False)
+    status_choices = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('completed', 'Завершена'),
+    ]
+    status = models.CharField(verbose_name='Статус', max_length=20, choices=status_choices, default='new')
+    due_date = models.DateField(verbose_name='Дедлайн', blank=True, null=True)
