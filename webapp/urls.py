6af122a43a1f6e80edf366f0686c3841804107a5
DEMOHCHIK <smnvmaks@gmail.com>
2023-11-27 19:11:22 +0600

    Добавил шаблон, представление + url для страницы task_list.html + реализовал удаление задач

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from webapp.views import home
+from webapp.views import home, task_list, task_delete
 
 
 urlpatterns = [
-    path('', home)
+    path('', home),
+    path('task_list/', task_list, name='task_list'),
+    path('task_delete/<int:task_id>/', task_delete, name='task_delete')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, redirect
+from .models import Task
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+
+def task_delete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('task_list')
