1ea43d30b508d291bb52db8feb98934dedade470
DEMOHCHIK <smnvmaks@gmail.com>
2023-11-28 14:53:34 +0600

    Добавил шаблон, представление + url для страницы add_task.html

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from webapp.views import home, task_list, task_delete
+from webapp.views import home, task_list, task_delete, add_task
 
 
 urlpatterns = [
     path('', home),
     path('task_list/', task_list, name='task_list'),
-    path('task_delete/<int:task_id>/', task_delete, name='task_delete')
+    path('task_delete/<int:task_id>/', task_delete, name='task_delete'),
+    path('add_task/', add_task, name='add_task')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -15,6 +15,20 @@ def task_list(request):
     return render(request, 'task_list.html', {'tasks': tasks})
 
 
+def add_task(request):
+    if request.method == 'POST':
+        description = request.POST['description']
+        due_date = request.POST['date']
+        status = request.POST['status']
+
+        task = Task.objects.create(description=description, due_date=due_date, status=status)
+        task.save()
+
+        return redirect('task_list')
+    else:
+        return render(request, 'add_task.html')
+
+
 def task_delete(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
