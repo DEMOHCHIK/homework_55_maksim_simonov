ff3e332a69b2ed03b961d709bbafc1f6eea48e5c
DEMOHCHIK <smnvmaks@gmail.com>
2023-12-08 10:36:10 +0600

    Реализовал редактирование и создание задачи с использованием формы

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from webapp.views import home, task_list, task_delete, add_task, task_detail
+from webapp.views import home, task_list, task_delete, add_task, task_detail, edit_task
 
 
 urlpatterns = [
     path('', home),
     path('task_list/', task_list, name='task_list'),
     path('task_delete/<int:pk>/delete/', task_delete, name='task_delete'),
+    path('task/<int:pk>/edit/', edit_task, name='edit_task'),
     path('add_task/', add_task, name='add_task'),
     path('task/<int:pk>/', task_detail, name='task_detail')
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
+from .forms import TaskForm
 
 
 def home(request):
@@ -17,25 +18,31 @@ def task_list(request):
 
 def add_task(request):
     if request.method == 'POST':
-        description = request.POST['description']
-        due_date = request.POST['date']
-        status = request.POST['status']
-        full_description = request.POST['full_description']
-        if due_date == "":
-            due_date = None
-
-        task = Task.objects.create(description=description, due_date=due_date, status=status, full_description=full_description)
-        task.save()
-
-        return redirect('task_list')
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
     else:
-        return render(request, 'add_task.html')
+        form = TaskForm()
+    return render(request, 'add_task.html', {'form': form})
 
 
 def task_detail(request, pk):
     task = get_object_or_404(Task, pk=pk)
     task.status = dict(Task.STATUS_CHOICES)[task.status]
     return render(request, 'task_detail.html', {'task': task})
+
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'edit_task.html', {'form': form, 'task': task})
 
 
 def task_delete(request, pk):
